def0cbe1cfa33313687b484f677966540eaaa1d7
John Krauss <irving.krauss@gmail.com>
2012-06-01 00:52:16 -0400

    new research

---
+++ research/geocoder.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# A very basic geocoder to get addresses from locations.json
+
+from geopy import geocoders
+import json
+import time
+
+geocoder = geocoders.Google()
+locations = json.load(open('locations.json', 'r'))
+output = {}
+errors = {}
+
+for l in locations:
+    addr = l['address']
+    try:
+        result = geocoder.geocode(addr)
+        print("%s: %s" % (addr, result))
+        output[addr] = result
+    except Exception as e:
+        print("%s: %s" % (addr, e))
+        errors[addr] = str(e)
+    time.sleep(2)
+
+json.dump(output, open('output.json', 'w'))
