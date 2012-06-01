ace6bc3ec6a03ef373bcd8b577c163686698c291
John Krauss <irving.krauss@gmail.com>
2012-06-01 00:52:30 -0400

    google closure doesnt know about json

---
+++ client/externs/json.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * JSON
+ */
+var JSON = {};
+
+/**
+ * @param {string} str
+ * @returns {(Object|Array)}
+ */
+JSON.parse = function (str) {};
+
+/**
+ * @param {(Object|Array)} obj
+ * @returns {string}
+ */
+JSON.stringify = function (obj) {};
